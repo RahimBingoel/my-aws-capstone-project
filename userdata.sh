5350cddee2d38e6c794af23b593d79c8f2c2df05
RahimBingol <63608928+RahimBingoel@users.noreply.github.com>
2023-05-09 15:53:26 +0200

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -5,11 +5,11 @@ apt-get install python3 -y
 cd /home/ubuntu/
 TOKEN="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"
 git clone https://$TOKEN@github.com/RahimBingoel/my-aws-capstone-project.git
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>
+cd /home/ubuntu/capstone
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
+cd /home/ubuntu/capstone/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
